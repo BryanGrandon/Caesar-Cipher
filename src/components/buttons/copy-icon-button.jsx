fa75ae92c3381717bb7bffc92106c5d774b2f53c
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-06-06 15:17:15 -0400

    feat(web):add copy-icon-button component

---
+++ src/components/buttons/copy-icon-button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { IoCopyOutline, IoCopy } from "react-icons/io5";
+
+function CopyIconButton({ onClick, value, className }) {
+  return (
+    <button onClick={onClick} value={value} className={className}>
+      <IoCopyOutline className="icon icon-copy-outline" />
+      <IoCopy className="icon icon-copy" />
+    </button>
+  );
+}
+export default CopyIconButton;
